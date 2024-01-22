5292ea409c9fa6a617491c61a4ee25ac4c9a8fdf
Abagado <147521764+Abagado@users.noreply.github.com>
2024-01-22 21:30:55 +1000

    fix auth + registration

--- GoTri/urls.py
+++ GoTri/urls.py
@@ -1,26 +1,23 @@
-"""
-URL configuration for GoTri project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from main.views import LobbyViewSet, MoveViewSet
 from register import views as v
 
+
+router = DefaultRouter()
+router.register('lobbies', LobbyViewSet)
+router.register('moves', MoveViewSet)
+
+urlpatterns = [
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("register/", v.register, name="register"),  # <-- added
-    path('', include("main.urls"), name="index"),
-    path('', include("django.contrib.auth.urls")),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path("register/", v.register, name="register"),  # <-- added
+    # path('', include("django.contrib.auth.urls")),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
 ]
--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,3 @@ from rest_framework.routers import DefaultRouter
 from .views import LobbyViewSet, MoveViewSet
 
 from . import views
-router = DefaultRouter()
-router.register('lobbies', LobbyViewSet)
-router.register('moves', MoveViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
