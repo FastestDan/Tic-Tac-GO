56b308e7f60e25f4bb677f0aa824498b4f795484
Abagado <147521764+Abagado@users.noreply.github.com>
2024-01-24 05:39:50 +1000

    Update models.py

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Lobby(models.Model):
     name = models.CharField(max_length=200)
-    guest = models.CharField(max_length=200)
+    guest = models.CharField(max_length=200, blank=True)
     objects = models.Manager()
 
     def __str__(self):
